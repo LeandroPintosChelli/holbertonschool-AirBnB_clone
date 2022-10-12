ffd5187626d399f510a2137fe48053d89bee8aba
Raidenlea <86613539+Raidenlea@users.noreply.github.com>
2022-10-12 12:13:41 -0300

    Add test id and to_dict

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,6 +11,18 @@ class TestBaseModel(unittest.TestCase):
 
     def test_base_model_1(self):
         Base = BaseModel()
-        Base.updated_at
+        var = Base.updated_at
         sleep(0.01)
         Base.save()
+        self.assertLess(var, Base.updated_at)
+
+    def test_id(self):
+        self.assertEqual(type(BaseModel().id), str)
+    
+    def test_to_dict(self):
+        Base = BaseModel()
+        Base_dict = dict(Base.__dict__)
+        Base_dict["__class__"] = "BaseModel"
+        Base_dict["created_at"] = Base_dict["created_at"].isoformat()
+        Base_dict["updated_at"] = Base_dict["updated_at"].isoformat()
+        self.assertEqual(Base_dict, Base.to_dict())
