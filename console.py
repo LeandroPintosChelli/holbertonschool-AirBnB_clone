5765ed3df94c2c6f5a0372f66fbc03192ecccc19
LeandroPintos <leandropintos162@gmail.com>
2022-10-13 10:44:09 -0300

    Add create to task 7

--- console.py
+++ console.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """Defines the HBnB console."""
 import cmd
+import imp
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.engine.file_storage import FileStorage
+from models.__init__ import storage
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import user
+
 
 
 class HBNBCommand(cmd.Cmd):
@@ -11,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = "(hbnb) "
+    Classes = ["State", "City", "Amanity", "Place", "Review"]
 
     def do_quit(self, args):
         """Quit command to exit the program."""
@@ -18,12 +29,24 @@ class HBNBCommand(cmd.Cmd):
 
     def do_EOF(self, line):
         """EOF signal to exit the program."""
-        print("")
+        print("")("")
         exit()
 
     def emptyline(self):
         """Do nothing when receiving an empty line."""
         pass
+
+    def do_create(self, args):
+        """Creates an instance"""
+        if args is None:
+            print("")("** class name missing **")
+        else: 
+            if args in self.Classes:
+                new = eval("{}()".format(args))
+                new.save()
+                print("")("{}".format(args.id))
+            else:
+                print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
