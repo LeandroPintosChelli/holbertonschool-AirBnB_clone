94376a6c7463691c8596a096ba85c8132f978c15
LeandroPintos <leandropintos162@gmail.com>
2022-10-13 13:16:40 -0300

    Fix task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,7 +46,7 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
         from models.state import State
-        from models.user import user
+        from models.user import User
         from models.amenity import Amenity
         from models.city import City
 
@@ -56,5 +56,5 @@ class FileStorage:
                     "Place": Place,
                     "Review": Review,
                     "State": State,
-                    "User": user}
+                    "User": User}
         return Classes
--- models/user.py
+++ models/user.py
@@ -4,7 +4,7 @@
 from models.base_model import BaseModel
 
 
-class user(BaseModel):
+class User(BaseModel):
     """Class representing an user"""
     email = ""
     password = ""
