049180a0b31282cc69dd564d49231ff31167e39b
LeandroPintos <leandropintos162@gmail.com>
2022-10-14 09:41:13 -0300

    Add comments

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,31 +13,38 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """Returns __objects dictionary."""
+        """Returns the list of objects of a class."""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets new obj in __objects dictionary."""
+        """Sets a new instance or object and adds it to the __objects dict."""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
         """Serialzes __objects to JSON file."""
         new_dict = {}
-
+        # Store the objects dictionary
+        # provided by the 'to_dict' method.
         for key, obj in self.__objects.items():
+            # a copy of the dict is created
             new_dict[key] = obj.to_dict()
+        # Convert the dict into a json file
         with open(self.__file_path, 'w') as f:
             json.dump(new_dict, f)
 
     def reload(self):
-        """If json file exists, convert obj dicts back to instances"""
+        """If json file exists, read it and 
+        convert object dicts back to instances"""
         try:
+            # Open the file 
             with open(self.__file_path, 'r') as f:
+                # Converts the file into a dictionary
                 new_obj = json.load(f)
                 for i in new_obj.values():
                     cls_name = i["__class__"]
                     del i["__class__"]
+                    # Create the objects and stores them in the class variable
                     self.new(eval(cls_name)(**i))
         except FileNotFoundError:
             pass
